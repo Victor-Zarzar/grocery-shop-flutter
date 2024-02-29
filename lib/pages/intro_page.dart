7150f9f0dc3307bf63a662edc9a51033a8965e0f
victorzarzar <victorzarzar58@gmail.com>
2024-02-29 11:38:04 -0300

    updates in intropage

--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -10,6 +10,29 @@ class IntroPage extends StatefulWidget {
 class _IntroPageState extends State<IntroPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+     body: Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(left: 80.0, right: 80, bottom: 40, top: 160),
+          child: Image.asset('lib/assets/images/avocado.png'),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Text('We deliver groceries at your doorstep', textAlign: TextAlign.center, style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+          ),
+        ),
+        Text('Fresh items everyday'),
+        Container(
+          decoration: BoxDecoration(
+            color: Colors.deepPurple,
+            borderRadius: BorderRadius.circular(12),
+          ),
+          padding: EdgeInsets.all(24),
+          child: Text('Get Started', style: TextStyle(color: Colors.white),),
+        )
+      ],
+     ),
+    );
   }
 }
