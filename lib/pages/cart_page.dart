fc217cb5a6415d251b3ad0cfad2eed68556bee97
victorzarzar <victorzarzar58@gmail.com>
2024-03-01 18:55:23 -0300

    updates in cart page, and home pag

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:grocery_shop_flutter/model/cart_model.dart';
 import 'package:provider/provider.dart';
 
@@ -9,12 +10,23 @@ class CartPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("My Cart"),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        iconTheme: const IconThemeData(color: Colors.black),
       ),
       body: Consumer<CartModel>(
         builder: (context, value, child) {
           return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
+              Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24.0),
+              child: Text(
+                "My Cart",
+                style: GoogleFonts.notoSerif(
+                    fontSize: 36, fontWeight: FontWeight.bold),
+              ),
+            ),
               Expanded(
                 child: ListView.builder(
                   itemCount: value.cartItems.length,
@@ -54,13 +66,39 @@ class CartPage extends StatelessWidget {
                   ),
                   padding: const EdgeInsets.all(24),
                   child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          const Text("Total Price", style: TextStyle(color: Colors.red),),
-                          Text('\$${value.calculateTotal()}')
+                          Text(
+                            "Total Price",
+                            style: TextStyle(color: Colors.green[100]),
+                          ),
+                          const SizedBox(height: 4),
+                          Text(
+                            '\$${value.calculateTotal()}',
+                            style: const TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold),
+                          ),
                         ],
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.green.shade100),
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        padding: const EdgeInsets.all(12),
+                        child: const Row(
+                          children: [
+                            Text(
+                              "Pay now",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            Icon(Icons.arrow_forward_ios, color: Colors.white),
+                          ],
+                        ),
                       ),
                     ],
                   ),
